ad5e232ee63fe970c3839d5b3afb655529dd033d
PaulGWu <24464317@life.hkbu.edu.hk>
2025-03-20 01:56:43 +0800

    upload chatbot file

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,32 +1,30 @@
-import configparser
 import requests
-
+import os
 
 class HKBU_ChatGPT():
-    def __init__(self, config_='./config.ini'):
-        if type(config_) == str:
-            self.config = configparser.ConfigParser()
-            self.config.read(config_)
-        elif type(config_) == configparser.ConfigParser:
-            self.config = config_
-
+    def __init__(self):
+        pass
+    
     def submit(self, message):
         conversation = [{"role": "user", "content": message}]
 
-        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
-
-        headers = {'Content-Type': 'application/json',
-                   'api-key': (self.config['CHATGPT']['ACCESS_TOKEN'])}
-        payload = {'messages': conversation}
+        url = ((os.environ['BASICURL']) +
+               "/deployments/" + (os.environ['MODELNAME']) +
+               "/chat/completions/?api-version=" +
+               (os.environ['APIVERSION']))
+        
+        headers = {
+            'Content-Type': 'application/json',
+            'api-key': (os.environ['ACCESS_TOKEN_LLM']) }
+        payload = { 'messages': conversation }
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
             data = response.json()
             return data['choices'][0]['message']['content']
         else:
             return 'Error:', response
-
-
-if __name__ == '__main__':
+        
+if __name__ == "__main__":
     ChatGPT_test = HKBU_ChatGPT()
 
     while True:
